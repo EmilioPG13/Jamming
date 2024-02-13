7bc97aac4465324dd6bb20172f8b503e0964d316
Emilio <emilio.pg95@gmail.com>
2024-02-12 20:37:13 -0600

    🚧 Implement Track Listing in the component Tree pt II

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,21 @@
 import React, { useState } from "react";
 import styles from "./App.module.css";
 
+function App() {
+    const [searchResults, setSearchResults] = useState([
+        {
+            name: "Example Track Name 1",
+            artist: "Example Track Artist 1",
+            album: "Example Track Album 1",
+            id: 1,
+        },
+        {
+            name: "Example Track Name 2",
+            artist: "Example Track Artist 2",
+            album: "Example Track Album 2",
+            id: 2,
+        },
+    ]);
+}
 
+export default App;
