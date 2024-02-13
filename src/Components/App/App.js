d3c625aee88a5178b8b7f81b7b7ef08c6a1c7257
Emilio <emilio.pg95@gmail.com>
2024-02-13 06:30:38 -0600

    🚀 Implement Playlist in The component Tree

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./App.module.css";
+import Playlist from "../Playlist/Playlist";
 
 function App() {
     const [searchResults, setSearchResults] = useState([
@@ -15,6 +16,27 @@ function App() {
             album: "Example Track Album 2",
             id: 2,
         },
+    ]);
+    const [playlistName, setPlaylistName] = useState("Example Playlist Name");
+    const [playlistTracks, setPlaylistTracks] = useState ([
+        {
+            name: "Example Playlist Name 1",
+            artist: "Example Playlist Artist 1",
+            album: "Example Playlist Album 1",
+            id: 11,
+        },
+        {
+            name: "Example Playlist Name 2",
+            artist: "Example Playlist Artist 2",
+            album: "Example Playlist Album 2",
+            id: 22,
+        },
+        {
+            name: "Example Playlist Name 3",
+            artist: "Example Playlist Artist 3",
+            album: "Example Playlist Album 3",
+            id: 33,
+        }
     ]);
 }
 
